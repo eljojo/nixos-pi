0684812f360ef675ac192ab2a323a3d1b6ee3b5f
José Albornoz <jojo@eljojo.net>
2022-08-29 19:05:45 -0400

    sanity updates

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,7 @@
   boot.loader.generic-extlinux-compatible.enable = true;
  
   # !!! Set to specific linux kernel version
-  boot.kernelPackages = pkgs.linuxPackages_5_4;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # !!! Needed for the virtual console to work on the RPi 3, as the default of 16M doesn't seem to be enough.
   # If X.org behaves weirdly (I only saw the cursor) then try increasing this to 256M.
@@ -35,66 +35,18 @@
 
   # systemPackages
   environment.systemPackages = with pkgs; [ 
-    vim curl wget nano bind kubectl helm iptables openvpn
-    python3 nodejs-12_x docker-compose ];
+    vim curl wget ];
 
   services.openssh = {
       enable = true;
       permitRootLogin = "yes";
   };
 
-  # Some sample service.
-  # Use dnsmasq as internal LAN DNS resolver.
-  services.dnsmasq = {
-      enable = false;
-      servers = [ "8.8.8.8" "8.8.4.4" "1.1.1.1" ];
-      extraConfig = ''
-        address=/fenrir.test/192.168.100.6
-        address=/recalune.test/192.168.100.7
-        address=/eth.nixpi.test/192.168.100.3
-        address=/wlan.nixpi.test/192.168.100.4
-      '';
-  };
-
-  # services.openvpn = {
-  #     # You can set openvpn connection
-  #     servers = {
-  #       privateVPN = {
-  #         config = "config /home/nixos/vpn/privatvpn.conf";
-  #       };
-  #     };
-  # };
-
-  programs.zsh = {
-      enable = true;
-      ohMyZsh = {
-          enable = true;
-          theme = "bira";
-      };
-  };
-
-
-  virtualisation.docker.enable = true;
-
   networking.firewall.enable = false;
   
-
-  # WiFi
-  hardware = {
-    enableRedistributableFirmware = true;
-    firmware = [ pkgs.wireless-regdb ];
-  };
   # Networking
   networking = {
     # useDHCP = true;
-    interfaces.wlan0 = {
-      useDHCP = false;
-      ipv4.addresses = [{
-        # I used static IP over WLAN because I want to use it as local DNS resolver
-        address = "192.168.100.4";
-        prefixLength = 24;
-      }];
-    };
     interfaces.eth0 = {
       useDHCP = true;
       # I used DHCP because sometimes I disconnect the LAN cable
@@ -103,42 +55,5 @@
       #  prefixLength = 24;
       #}];
     };
-
-    # Enabling WIFI
-    wireless.enable = true;
-    wireless.interfaces = [ "wlan0" ];
-    # If you want to connect also via WIFI to your router
-    wireless.networks."WIFI-SSID".psk = "wifipass";
-    # You can set default nameservers
-    nameservers = [ "192.168.100.3" "192.168.100.4" "192.168.100.1" ];
-    # You can set default gateway
-    defaultGateway = {
-      address = "192.168.100.1";
-      interface = "wlan0";
-    };
   };
-
-  # put your own configuration here, for example ssh keys:
-  users.defaultUserShell = pkgs.zsh;
-  users.mutableUsers = true;
-  users.groups = {
-    nixos = {
-      gid = 1000;
-      name = "nixos";
-    };
-  };
-  users.users = {
-    nixos = {
-      uid = 1000;
-      home = "/home/nixos";
-      name = "nixos";
-      group = "nixos";
-      shell = pkgs.zsh;
-      extraGroups = [ "wheel" "docker" ];
-    };
-  };
-  users.extraUsers.root.openssh.authorizedKeys.keys = [
-    # This is my public key
-     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDqlXJv/noNPmZMIfjJguRX3O+Z39xeoKhjoIBEyfeqgKGh9JOv7IDBWlNnd3rHVnVPzB9emiiEoAJpkJUnWNBidL6vPYn13r6Zrt/2WLT6TiUFU026ANdqMjIMEZrmlTsfzFT+OzpBqtByYOGGe19qD3x/29nbszPODVF2giwbZNIMo2x7Ww96U4agb2aSAwo/oQa4jQsnOpYRMyJQqCUhvX8LzvE9vFquLlrSyd8khUsEVV/CytmdKwUUSqmlo/Mn7ge/S12rqMwmLvWFMd08Rg9NHvRCeOjgKB4EI6bVwF8D6tNFnbsGVzTHl7Cosnn75U11CXfQ6+8MPq3cekYr lucernae@lombardia-N43SM"
-  ];
 }
